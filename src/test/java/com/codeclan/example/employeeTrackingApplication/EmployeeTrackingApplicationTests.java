c85e57312c4d9b84b9b3241b9098b844409a3de1
Karen Stewart <kastewart2002@googlemail.com>
2021-11-29 15:55:21 +0000

    first commit

---
+++ src/test/java/com/codeclan/example/employeeTrackingApplication/EmployeeTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeeTrackingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
